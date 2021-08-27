97582f50c547cfe6e04bda8b4b409f48e88e3bad
camila odorizzi <camilaodorizzi25@gmail.com>
2021-08-27 16:46:24 -0300

    Create php1.php

---
+++ SintaxePhp/php1.php
@@ -0,0 +1,12 @@
+<?php // bloco de abertura php
+// formas de apresentar conteudo
+ echo 'Curso php';
+ print "Curso php";
+
+ //echo é forma mais utilizada 
+ //formas de apresentação
+
+ echo'php';
+ echo"php";
+ echo("php");
+ 
