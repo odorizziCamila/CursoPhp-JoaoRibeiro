904e69bb12827e24df9fa07ec24394b3a1f8fde5
camila odorizzi <camilaodorizzi25@gmail.com>
2021-08-27 16:54:24 -0300

    Create phpHtml.php

---
+++ SintaxePhp/phpHtml.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" >
+        <title>Curso PHP</title>
+
+    </head>
+    <body>
+        <?php echo'teste1'?>
+        <br>
+        <?php echo"teste2"?>
+        <br>
+        <?= 'teste3'?>
+    
+    </body>
+
+</html>
