fe1ebb2fd054b1c4ba84ed5186e5ee7ff5e85dbe
camila odorizzi <camilaodorizzi25@gmail.com>
2021-08-28 15:53:03 -0300

    Create TiposDados.php

---
+++ SintaxePhp/TiposDados.php
@@ -0,0 +1,18 @@
+<?php
+
+
+#tipos de dados
+//as variaveis e php podem ter diferentes tipos de valores.
+//a mesma variável pode ter tipos diferentes de valores.
+$variavel =100;
+$vaiavel ="cem";
+
+//apesar de serem variáveis de tipod implicitos, é importante saber que:
+
+$inteiro = 100; //int
+$float =10,20; //float com casas decimais
+$bool=true; //verdadeiro ou falso
+$string="olá mundo";//string cadeia de caracteres
+$array =[1,2,3];//array-coleção de valores
+$pessoa= new Pessoa();//objeto com propriedade e métodos
+$nulo= null;//variavel com valor nulo.
