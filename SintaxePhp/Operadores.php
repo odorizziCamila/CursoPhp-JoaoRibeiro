15e74dacc4bec630ff56746546f530f8bcf68814
camila odorizzi <camilaodorizzi25@gmail.com>
2021-08-28 16:18:40 -0300

    Update Operadores.php

--- SintaxePhp/Operadores.php
+++ SintaxePhp/Operadores.php
@@ -13,3 +13,24 @@ $X=4-2;//subtração
 $x=4*2;//multiplicaçao
 $x=4/2;//divisão
 $x=4%2;//modulos(restante da divisão)
+
+
+echo 3462/7;
+echo '<br>';
+echo 3462%7;
+
+$a=10;
+$b=20;
+$resultado=$a+$b;
+echo '<br>';
+echo $resultado;
+$resultado= $a+100;
+echo '<br>';
+echo $resultado;
+$resultado=$a*$b;
+echo '<br>';
+echo $resultado;
+
+$resultado =1000+$a+10+$b;
+echo '<br>';
+echo $resultado;
