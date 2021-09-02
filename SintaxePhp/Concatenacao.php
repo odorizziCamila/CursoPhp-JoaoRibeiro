43a9a07bdb9a9783d1c5d791c170d9a488f7ad59
camila odorizzi <camilaodorizzi25@gmail.com>
2021-09-02 09:50:44 -0300

    Update Concatenacao.php

--- SintaxePhp/Concatenacao.php
+++ SintaxePhp/Concatenacao.php
@@ -1,1 +1,24 @@
 <?php
+// Concatenação de strings
+
+//É o processo que, atraves do código, permite juntar várias strings.
+//Existem dois operadores de concatenação:
+// sendo estes: . e .=
+
+$a = 'joao'.' '.'Ribeiro'; // o ponto irá ligar as diferentes strings.
+
+$b='Joao';
+$b.=' '.'Ribeiro';//.= faz a concatenação com o valor já existente na string.
+
+echo $b;
+echo'<br>';
+echo $a;
+//die();
+
+$nome ='Camila';
+$apelido='mila';
+$nome_completo =$nome.' '.$apelido;
+
+echo $nome_completo;
+die();
+
