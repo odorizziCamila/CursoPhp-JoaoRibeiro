0421afc06c7cbf7f3db3d638e1289f1e5d4925a4
camila odorizzi <camilaodorizzi25@gmail.com>
2021-09-03 12:03:47 -0300

    Create ExercicioString02.php

---
+++ SintaxePhp/ExercicioString02.php
@@ -0,0 +1,7 @@
+<?php
+//Defina as variaveis nome e apelido com os respectivos valores.
+//apresente o nome competo num paragrafo de html. 
+// no segundo paragrafo apresente o seguinte;
+// o nome tem x caracteres.
+//sendo x o calculo total de caracteres do seu nome.
+
