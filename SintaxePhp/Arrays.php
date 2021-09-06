26a5c95d8ef1838f76fc0acff92785d3499dba97
camila odorizzi <camilaodorizzi25@gmail.com>
2021-09-06 17:23:20 -0300

    Update Arrays.php

--- SintaxePhp/Arrays.php
+++ SintaxePhp/Arrays.php
@@ -22,6 +22,54 @@ $nomes = ['Joao','Camila','Bianca'];
 //Estes arrays são de indices númericos.
 //Para apresentar um dos seus valores, procedemos da seguinte forma:
 echo $valores[0];
-echo $nome[1];
+echo $n='<br>';
+echo $nomes[1];
 //Nunca esquecer que os arrays tem indice de base 0.
+echo $n;
+$dados = [
+    10=>100,
+    20=>200,
+    30=>300
+];
+echo $dados[10];
 
+// se quiser adicionar um novo elemento ao final da coleção, ele vai assumir o indice +1;
+$dados[]=400;//$dados[31];
+echo $n;
+
+//Sempre que queremos apresentar um valor de um array, usamos o seu indice.
+//Da mesma forma, podemos alterar o valo do array usando o seu indice.
+
+$valores =[10,20,30];
+$valores[1]=2000; //$valores=[10,2000,30];
+
+//podemos também ignorar o indice para adicionar um novo elemto ao array
+$valores[]=3000; //$valores =[10,20,30,3000];
+// Ou ainda 
+array_push($valores,5000);//$valores=[10,2000,30,3000,5000];
+
+//para apresentar valores de um array numa string, podemos fazer da seguinte forma:
+echo 'Os valores são:'.$valores[0].'e'.$valores[1];
+echo $n;
+echo "Os valores são: $valores[0] e $valores[1]";
+
+
+//Arrays associativos
+//Ao contrario dos arrays numéricos, as chaves são do tipo string
+
+$dados = [
+    'A'=>20,
+    'B'=>30,
+    'C'=>40,
+    'D'=>50
+];
+//ou
+$dados= [
+    'nome'=>'Joao',
+    'email'=>'joao@gmail.com',
+    'nacionalidade'=> 'Brasileiro'
+
+];
+
+
+]
