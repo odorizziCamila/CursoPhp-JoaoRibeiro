b81173e86c58b03b0ef862423a245ed22764671b
camila odorizzi <camilaodorizzi25@gmail.com>
2021-09-08 18:16:35 -0300

    Update Condicional.php

--- SintaxePhp/Condicional.php
+++ SintaxePhp/Condicional.php
@@ -10,7 +10,7 @@
 
 
 //Declarações/instruções condicionais
-//Declaração de IF - deine blocos de código que só são executados se a declaração fo verdadeira(true)
+//Declaração de IF - define blocos de código que só são executados se a declaração for verdadeira(true)
 
 $nome = 'Camila';
 if($nome=='Camila'){
@@ -23,4 +23,42 @@ if($idade<=20){
     echo 'Adolescente';
 } else {
     echo'Adulto';
+}
+
+
+//if..else if ... else
+$nota =5;
+if($nota<=2){
+    echo'nota fraca';
+} elseif($nota<=4){
+    echo'nota insuficiente';
+}elseif($nota<=6){// neste ponto do codigo ele executa e apresenta 
+    echo'nota positiva';
+}elseif($nota<=8){
+    echo'nota boa';
+}else{
+    echo'nota excelente';
+}
+
+//Uso de operadores lógicos
+
+$numero =20;
+if($numero>10 && $numero!=30){
+    //executa o código
+}else{
+    //executa outro código
+}
+
+//condições dentro de condições
+if($numero>0){
+    if($numero>=100){
+        echo 'O numero é igual ou maior que 100';
+    }else{
+        echo'O numero e positivo mais menor que cem';
+    }
+}
+    else{
+        echo'O número é negativo';
+    }
+
 }
