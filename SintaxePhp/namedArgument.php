a491b0357e803779b4646a00ce0a502bc94a8ad8
camila odorizzi <camilaodorizzi25@gmail.com>
2021-09-26 22:30:53 -0300

    commit

--- SintaxePhp/namedArgument.php
+++ SintaxePhp/namedArgument.php
@@ -12,3 +12,32 @@ echo '<br>';
 // No php 8 podemos fazer da seguinte forma:
 adicionar(c:1000, a:300);
 echo'<br>';
+
+// Informações adicionais sobre parametros e variadic
+
+// Uma função nunca pode ser chamada sem que os valores obrigatórios sejam fornecidos 
+//Exemplo:
+
+function funcao($a,$b){
+    
+}
+//funcao('teste');
+
+function outraFuncao($a=8){
+    echo"$a";// a variavel $a tera o mesmo valor de $x.
+    $x = func_get_arg(0);
+    $y = func_get_arg(1);
+    $z = func_get_arg(2);
+    echo"$x - $y - $z";
+    echo '<br>';
+    echo func_num_args(); //avalia a quantidade de argumentos qu foram passados na função.
+}
+outraFuncao(10,20,30);
+
+//variadic parameter
+function minhaFuncao(... $argumentos){
+    foreach($argumentos as $v){
+        echo"$v<br>";
+    }
+}
+minhaFuncao(10,20,30,40,50,60);
