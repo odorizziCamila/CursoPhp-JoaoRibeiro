5df44f009b6ee00ada73118d146cf72d0c5d9593
camila odorizzi <camilaodorizzi25@gmail.com>
2021-10-05 14:37:52 -0300

    commit

--- SintaxePhp/ObjetoPOO.php
+++ SintaxePhp/ObjetoPOO.php
@@ -9,7 +9,20 @@ class Humano{
     public $apelido ='Ribeiro';
 
     public function nomeCompleto(){
-        return $this->nome.''.$this->apelido;
+        $nome ='joao';
+        return $this->nome.' '.$this->apelido;
     }
+    
 }
-// As classes recorrem a utilização de Acess Modifiers - Niveis de acesso. Os niveis de acesso aos dados indiam se podemos ver os dados apenas dentro da classe, se podems 
+
+// As classes recorrem a utilização de Acess Modifiers - Niveis de acesso. Os niveis de acesso aos dados indiam se podemos ver os dados apenas dentro da classe, se podemos.
+
+// INSTANCIAR UM OBJETO
+
+//Um objeto é uma variável criada a partir de uma classe.
+//Instaciar um objeto criar um objeto a partir de uma classe atribuindo á variável a expressão new e o nome da classe.
+
+$homem = new Humano();
+// Com a implementação anterior podemos agora aceder as propriedades e métodos.
+echo $homem -> nomecompleto();// Camila Ribeiro
+// 
