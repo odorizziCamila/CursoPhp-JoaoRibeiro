6941581cd290b2f82190e20d8b82038890b65a2e
camila odorizzi <camilaodorizzi25@gmail.com>
2021-10-06 23:08:55 -0300

    commit

---
+++ SintaxePhp/ExpressaoFinalPOO.php
@@ -0,0 +1,33 @@
+<?php
+//final
+
+// Para impedir q uma classe derivada possa fazer override de método, podemos utilizar a expressao FINAL da seguinte forma:
+
+use Humano as GlobalHumano;
+
+class Veiculo{
+    final function mover(){
+
+    }
+}
+class bicicleta extends Veiculo{
+    //function mover(){// utilizando o final na função mover da class veiculo não será possivel a criação da função mover dentro da classe que extender da mesma.
+        
+
+    }
+
+
+// Podemos inclusive definir que uma classe como não sendo passível de ser herdada colocando FINAL antes da classe.
+//Conforme exemplo a seguir:
+
+final class Humano{
+     function teste(){
+        echo'teste';
+    }
+}
+
+class Homem extends Humano{
+
+}
+$tst = new Homem();
+$tst->teste();
