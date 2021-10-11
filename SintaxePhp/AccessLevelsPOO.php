089db8891f7a7b77109e0ed40d0b43ac898c697e
camila odorizzi <camilaodorizzi25@gmail.com>
2021-10-11 15:41:29 -0300

    commit

--- SintaxePhp/AccessLevelsPOO.php
+++ SintaxePhp/AccessLevelsPOO.php
@@ -9,9 +9,9 @@ class MinhaClasse{
 }
 
 $variavel = new MinhaClasse();
-$variavel->$a=10; // posivel
-$variavel->$b =30; // não é possível
-$variavel->$c=40; // não é possível
+$variavel->a=10; // posivel
+//$variavel->$b =30; // não é possível
+//$variavel->$c=40; // não é possível
 
 // ACCESS PUBLIC
 // Os membros publicos (propriedades e métodos) de uma classe, estão sempre acessiveis, se criarmos um objeto a partir da classe, temos acesso direto a esses elementos.
@@ -21,10 +21,11 @@ class TudoPublico{
 
     public function metodo(){
         echo'método publico';
+        echo'<br>';
     }
 }
 $objt = new TudoPublico();
-$objt->$propriedade ='a';
+$objt->propriedade ='a';
 $objt->metodo();
 
 
@@ -37,9 +38,7 @@ class ClasseTeste{
     protected $protegida ="b";
     private $privada ="c";
 }
-function info(){
-    return "{$this->publica}";
-}
+
 
 class ClasseDerivada extends ClasseTeste{
     function teste(){
@@ -50,8 +49,39 @@ class ClasseDerivada extends ClasseTeste{
 }
 $obj = new ClasseTeste();
 $obj->publica ="1"; // possivel
-$obj->protected ="2"; //  gera erro
-$obj->privada ="3"; // gera erro
+//$obj->protected ="2"; //  gera erro
+//$obj->privada ="3"; // gera erro
+
+$obj = new ClasseDerivada();
+$obj->publica="1";
+
+// na classe derivada que extends da classe tesste temos a seguinte situação
+
+$objt1 = new ClasseDerivada();
+$objt1 -> publica ="1";// possivel
+//$objt1 -> protegida ="2";//gera erro
+//$objt1 -> privada ="3";// gera erro
 
 
+// ACESS PRIVATE 
+// Só pode ser visivel dentro da propria classe.
+//Não é visivel nos objetos instanciados nem noutras classes herdadas.
+//-----------------------
+
+//Ao contrario das propriedades, os métodos não necessitam de ter umm nível de acesso especificado claramente.
+// Se não for identificado, por dentro  o método é publico.
+
+
+class Teste{
+    private $valor="a";
+
+
+    function mover(){
+        // codigo aqui
+        echo $this->valor; 
+    }
+}
+$a = new Teste();
+$a->mover(); //possivel
+//$a->valor='1';//não é possivel
 
---
+++ SintaxePhp/varObjectAccess.php
@@ -0,0 +1,29 @@
+<?php
+//VAR Keyword
+// tem o mesmo comportamento de public, mas apenas existe por retrocompatibilidade devido ao codigo escrito antes do PHP 5. Não é recomendável usar porque poderá em breve passar a ser não suportado.
+
+class Homem{
+    var $nome, $apelido;
+
+}
+$eu = new Homem();
+$eu->nome='Joao';
+$eu->apelido='Ribeiro';
+
+// OBJECT ACCESS
+
+// No php, um objeto instanciado a partir de uma classe pode aceder a elementos privados e protegidos de outro objeto, criado a partir da mesma clase.
+// Este comportamento não acontece na maior parte das linguagem de programação
+
+class Humano{
+
+    private $nome='a';
+    function setPrivate($objeto, $valor){
+        $objeto->nome=$valor;
+    }
+    function apresentar(){
+        echo $this->nome;
+    }
+}
+
+$a = new Humano();
