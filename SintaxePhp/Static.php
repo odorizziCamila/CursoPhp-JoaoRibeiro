e2aa319fd05097a09b0da2bbb010f74ad42c0fce
camila odorizzi <camilaodorizzi25@gmail.com>
2021-10-13 15:44:06 -0300

    commit

---
+++ SintaxePhp/Static.php
@@ -0,0 +1,35 @@
+<?php
+
+// Static 
+// A palavra chave static pode ser usada para declarar propriedades e métodos de uma classe que podem ser acedidos sem que sejam necessário criar um objeto a partir dessa classe.
+
+class Teste{
+
+
+    // instance members - um membro por cada objeto criado a partir da classe
+    public $nome;
+    function teste(){
+        //codigo
+    }
+
+    // static ou class members - apenas existentes uma vez na classe
+    static $idade;
+    static function mover(){
+        //codigo aqui
+    }
+}
+
+// Exemplo pratico 
+
+class operacoes{
+    static $valor01;
+    static $valor02;
+
+    static function adicinar(){
+        return self::$valor01+self::$valor02;
+
+    }
+}
+Operacoes::$valor01=10;
+Operacoes::$valor02=20;
+echo Operacoes::adicinar();
--- SintaxePhp/gettersSetters.php
+++ SintaxePhp/gettersSetters.php
@@ -11,11 +11,63 @@ class Pessoa{
     public $idade;
 }
 $eu = new Pessoa();
-$eu->idade='Olá mundo';// isto é possivel porque o PHP não é strongly typed.
+echo $eu->idade=30;
+echo'<br>';
+echo $eu->idade='Olá mundo';// isto é possivel porque o PHP não é strongly typed.
 
 // Para definir corretamente este tipo de situação, podemos usar para definir e ir buscar os valores das propriedades privadas. Estes métodos permitem garantir que a integridade das propriedades seja mantida.
 
 class Humano1{
     private $idade;
     
+}
+$eu->idade='camila';// não é possível efetuar, pois a propriedade é privada
+
+// exemplo de implementaçao com accesso privado
+
+class Pessoa1{
+    private $idade=0;
+
+    public function setIdade($valor){
+        // verifica se $valor é numérico
+        if(is_numeric($valor)){
+            $this->idade=$valor;
+        }
+    }
+    public function getIdade(){
+        return "A idade é ". $this->idade;
+    }
+}
+$pessoa = new Pessoa1();
+$pessoa->setIdade(30);
+echo'<br>';
+echo $pessoa->getIdade();
+
+//----------------------------------------------
+
+class Tempo{
+
+    private $segundos;
+
+
+    function setSegundos($valor){
+        if(!is_numeric($valor)|| $valor<0){
+            $this->segundos =0;
+        }else{
+            $this->segundos=$valor;
+        }
+    }
+
+    function getMinutos(){
+        return "". $this->segundos/60;
+    }
+
+    function setMinutos($valor){
+        if($valor==0){
+            $this->segundos=0;
+        }else {
+            $this->segundos=$valor=60;
+        }
+
+    }
 }
